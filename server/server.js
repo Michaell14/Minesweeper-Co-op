4629245e20be011b80d435ae501365d9e9ca824e
Michael <limichael909@gmail.com>
2024-12-28 15:48:00 -0500

    no trailing zeroes

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:3000", // Development
-            "https://www.minesweepercoop.com/"
+            "https://www.minesweepercoop.com"
         ]
     }
 });
